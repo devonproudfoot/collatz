39f0af0cad188a089453be2ad0c9a42227befe3a
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-09-27 00:09:29 +0000

    It works

---
+++ README.md
@@ -0,0 +1,1 @@
+Repo for the Collatz coding challenge
--- collatz.rb
+++ collatz.rb
@@ -1,4 +1,27 @@
-def collatz_length(starting_number)
-  count = 0
+def find_collatz(num)
+  count = 1
 
+  while num > 1
+    if num % 2 == 0
+      num = num / 2
+    else
+      num = 3 * num + 1
+    end
+    count += 1
+  end
+  return count
 end
+
+def find_highest_collatz(start, stop)
+  top_collatz = 0
+  (start..stop).each do |i|
+    collatz = find_collatz(i)
+
+    if collatz > top_collatz
+      top_collatz = i
+    end
+  end
+  return top_collatz
+end
+
+puts find_highest_collatz(1, 1000000)
