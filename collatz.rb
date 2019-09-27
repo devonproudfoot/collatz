be5a40d9cd355ceb785b823e130140b7596f767e
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-09-27 18:20:44 +0000

    small tweaks

--- collatz.rb
+++ collatz.rb
@@ -1,5 +1,5 @@
 # Takes the argument and returns the length of the collatz sequence
-def collatz_length(num, known_collatz_lengths)
+def collatz_length(num, known_collatz_lengths={})
   count = 1
 
   while num != 1
@@ -7,10 +7,9 @@ def collatz_length(num, known_collatz_lengths)
     # If num is a key in the known_collatz_lengths hash then add value to count and return value
     if known_collatz_lengths[num]
       return count + known_collatz_lengths[num]
-    end
-    if num.even?
+    elsif num.even?
       num = num / 2
-    else
+    elsif num.odd?
       num = 3 * num + 1
     end
     count += 1
